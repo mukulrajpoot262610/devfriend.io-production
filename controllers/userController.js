afb3d756ba1c8b6c90d3f0d36c5b27c170861b69
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2021-09-25 09:50:42 +0530

    Added ENV and firebase routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,14 @@
-export const getAllUser = (req, res) => {
-    res.status(200).json({
-        message: 'All Rooms',
-        success: true,
-    })
+// @route  GET api/firebase/user
+// @desc   Auth User via firebase and get Profile
+// @access PRIVATE
+export const regsiterUser = async (req, res) => {
+    res.status(200).json(req.currentUser)
 }
+
+// @route  GET api/firebase/profile
+// @desc   Auth User via firebase and get Profile
+// @access PRIVATE
+export const GetProfile = async (req, res) => {
+    res.status(200).json(req.currentUserProfile)
+}
+
