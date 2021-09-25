3eeaa52795932c2fa69706c42e85f88d21375bb7
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2021-09-25 17:24:14 +0530

    Added Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import 'tailwindcss/tailwind.css'
+import Navbar from '../components/Navbar'
+import '../style/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#fff',
+        seondary: '#2a2b2f',
+        normal: {
+          100: '#eff4ff',
+          900: '#1890ff',
+        },
+        info: {
+          100: '#f5e7c5',
+          900: '#ffba3a',
+        },
+        danger: {
+          100: 'rgb(220, 20, 60, 0.1)',
+          900: '#dc143c',
+        },
+      }
+    },
   },
   variants: {
     extend: {},
