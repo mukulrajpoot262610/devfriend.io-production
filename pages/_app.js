0e897f8d36c3558ceffefe85d74c09380c382935
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2021-09-26 18:44:10 +0530

    Removed Firebase

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,15 @@ import '../style/global.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import store from '../store/store'
-import FirebaseAuthState from '../config/FirebaseAuthState'
+// import FirebaseAuthState from '../config/FirebaseAuthState'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        <FirebaseAuthState>
-          <Navbar />
-          <Component {...pageProps} />
-          <Footer />
-        </FirebaseAuthState>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
       </Provider>
     </>
   )
